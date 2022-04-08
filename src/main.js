9dac106986f66f1ca05ffbceea1ecb36630005b1
KatherineEvans <Evans.Katherine.E@gmail.com>
2022-04-08 15:52:50 -0500

    amplify

--- src/main.js
+++ src/main.js
@@ -4,4 +4,10 @@ import router from "./router";
 import store from "./store";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
-createApp(App).use(store).use(router).mount("#app");
+import Amplify, * as AmplifyModules from "aws-amplify";
+import { AmplifyPlugin } from "aws-amplify-vue";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
+createApp(App).use(store).use(router).use(AmplifyPlugin, AmplifyModules).mount("#app");
