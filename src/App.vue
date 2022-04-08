047cae7b6d8c2d23434f95b34e3c70dfcbab5f1c
KatherineEvans <Evans.Katherine.E@gmail.com>
2022-04-08 14:04:49 -0500

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-dark bg-dark">
+    <!-- Navbar content -->
   </nav>
   <router-view />
 </template>
