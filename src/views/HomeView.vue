245fc22d097f7a0b6d9dbf58d46eb93781d58582
KatherineEvans <Evans.Katherine.E@gmail.com>
2022-04-16 18:44:35 -0500

    udpates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img src="../assets/morning.svg" />
   </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
+  data: function () {},
+  created() {},
+  mounted() {},
+  computed: {
+    imageUrl() {
+      let now = new Date();
+      let hours = now.getHours();
+      if (hours >= 6 && hours <= 14) {
+        console.log(hours, "morning");
+        return "../assets/morning.svg";
+      } else if (hours >= 15 && hours <= 18) {
+        console.log(hours, "afternoon");
+        return "../assets/afternoon.svg";
+      } else {
+        console.log(hours, "evening");
+        return "../assets/evening.svg";
+      }
+    },
+  },
+  methods: {},
 };
 </script>
