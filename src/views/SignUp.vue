24cdb558313b3f0468876852770ab761182ba3c7
KatherineEvans <Evans.Katherine.E@gmail.com>
2022-07-30 17:24:48 -0500

    updates to signup page - includes pronouns

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -13,12 +13,25 @@
     <div class="row pb-5 px-5 m-auto">
       <form action="submit-signup">
         <div class="card mt-5 py-3 px-5">
-          <div class="input-group row mb-3">
-            <label class="form-label pl-0">First Name:</label>
-            <input type="text" class="form-control" />
+          <div class="row">
+            <div class="col-6">
+              <div class="input-group row mb-3">
+                <label class="form-label pl-0">First Name:</label>
+                <input type="text" class="form-control" />
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="input-group row mb-3">
+                <label class="form-label pl-0">Last Name:</label>
+                <input type="text" class="form-control" />
+              </div>
+            </div>
           </div>
           <div class="input-group row mb-3">
-            <label class="form-label pl-0">Last Name:</label>
+            <label class="form-label pl-0">
+              Phone Number:
+              <span class="text-secondary"><small>(optional)</small></span>
+            </label>
             <input type="text" class="form-control" />
           </div>
           <div class="input-group row mb-3">
@@ -32,6 +45,26 @@
           <div class="input-group row mb-3">
             <label class="form-label pl-0">Password Confirmation:</label>
             <input type="password" class="form-control" />
+          </div>
+          <div class="input-group row mb-3">
+            <label class="form-label pl-0">Pronouns:</label>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+              <label class="form-check-label" for="flexRadioDefault1">She/Her/Hers</label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
+              <label class="form-check-label" for="flexRadioDefault2">He/Him/His</label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
+              <label class="form-check-label" for="flexRadioDefault2">They/Them/Theirs</label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
+              <label class="form-check-label" for="flexRadioDefault2">Other</label>
+            </div>
+            <input type="text" class="form-control" />
           </div>
           <button
             style="max-width: 150px; background-color: #0b0b35; border-color: #0b0b35; color: white"
@@ -76,6 +109,6 @@ img {
   margin: auto;
 }
 .input-group > .form-control {
-  border-radius: 5 !important;
+  border-radius: 5px !important;
 }
 </style>
