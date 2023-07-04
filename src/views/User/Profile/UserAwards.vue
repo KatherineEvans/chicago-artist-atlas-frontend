27c0f142c8b02704aa0dd631c686fef4450069ec
Katherine <Evans.Katherine.E@gmail.com>
2023-07-04 16:37:14 -0500

    fix nav

--- src/views/User/Profile/UserAwards.vue
+++ src/views/User/Profile/UserAwards.vue
@@ -43,14 +43,23 @@
       </div>
     </div>
 
-    <div class="mt-10 flex items-center justify-end gap-x-6">
-      <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-      <button
-        type="submit"
-        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Save
-      </button>
+    <div class="mt-10 flex justify-between gap-x-6">
+      <span>
+        <a
+          href="/user/profile/performances"
+          class="mr-3 rounded-md bg-slate-600 px-3 py-2 text-sm font-semibold text-white hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600 no-underline hover:no-underline"
+        >
+          Back
+        </a>
+        <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+      </span>
+      <span>
+        <button
+          class="ml-3 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 no-underline no-underline hover:no-underline"
+        >
+          Save
+        </button>
+      </span>
     </div>
   </form>
 </template>
