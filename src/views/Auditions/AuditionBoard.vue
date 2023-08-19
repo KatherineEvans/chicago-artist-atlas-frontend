c94d577cd67d849ee0fb4eda1e9add3386c75b5c
Katherine <Evans.Katherine.E@gmail.com>
2023-08-19 08:33:56 -0500

    updates links

--- src/views/Auditions/AuditionBoard.vue
+++ src/views/Auditions/AuditionBoard.vue
@@ -10,72 +10,81 @@
       </div>
     </div>
     <div class="container main">
-      <div class="row my-2 py-2 no-gutters">
-        <!-- SEARCH / FILTER BAR -->
-        <div class="d-flex flex-row-reverse p-4">
-          <div>
-            <label for="union-status" class="mr-2 sm:text-sm sm:leading-6">Union Status:</label>
-            <Menu id="union-status" as="div" class="relative inline-block text-left">
-              <div>
-                <MenuButton
-                  class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  Any
-                  <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-                </MenuButton>
-              </div>
-
-              <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
+      <!-- SEARCH / FILTER BAR -->
+      <div class="d-flex flex-row-reverse p-4">
+        <div>
+          <label for="union-status" class="mr-2 sm:text-sm sm:leading-6">Union Status:</label>
+          <Menu id="union-status" as="div" class="relative inline-block text-left">
+            <div>
+              <MenuButton
+                class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
               >
-                <MenuItems
-                  class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                >
-                  <div class="py-1">
-                    <MenuItem v-slot="{ active }">
-                      <button
-                        class="w-100 text-left"
-                        :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2']"
-                      >
-                        Non-Equity
-                      </button>
-                    </MenuItem>
-                    <MenuItem v-slot="{ active }">
-                      <button
-                        class="w-100 text-left"
-                        :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2']"
-                      >
-                        Equity
-                      </button>
-                    </MenuItem>
-                  </div>
-                </MenuItems>
-              </transition>
-            </Menu>
-          </div>
-          <div class="flex flex-1 items-center justify-center pr-3 lg:ml-6 lg:justify-end">
-            <div class="w-full max-w-lg lg:max-w-xs">
-              <label for="search" class="sr-only">Search Production or Company</label>
-              <div class="relative">
-                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                  <MagnifyingGlassIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                Any
+                <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+              </MenuButton>
+            </div>
+
+            <transition
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95"
+            >
+              <MenuItems
+                class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <button
+                      class="w-100 text-left"
+                      :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2']"
+                    >
+                      Non-Equity
+                    </button>
+                  </MenuItem>
+                  <MenuItem v-slot="{ active }">
+                    <button
+                      class="w-100 text-left"
+                      :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2']"
+                    >
+                      Equity
+                    </button>
+                  </MenuItem>
                 </div>
-                <input
-                  id="search"
-                  name="search"
-                  class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="Search Production or Company"
-                  type="search"
-                />
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
+        <div class="flex flex-1 items-center justify-center pr-3 lg:ml-6 lg:justify-end">
+          <div class="w-full max-w-lg lg:max-w-xs">
+            <label for="search" class="sr-only">Search Production or Company</label>
+            <div class="relative">
+              <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                <MagnifyingGlassIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
               </div>
+              <input
+                id="search"
+                name="search"
+                class="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Search Production or Company"
+                type="search"
+              />
             </div>
           </div>
         </div>
+      </div>
+      <div class="row my-2 py-2 no-gutters h-screen" v-if="isLoading">
+        <div class="loading-spinner-container">
+          <div class="loading-spinner m-auto">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <div v-else class="row my-2 py-2 no-gutters">
         <!-- AUDITIONS -->
         <div class="row my-2 py-2 pl-4">
           <AuditionCard
@@ -113,6 +122,7 @@ export default {
       auditions: [],
       currentAuditionId: null,
       heart: false,
+      isLoading: true,
     };
   },
   computed: {
@@ -158,6 +168,7 @@ export default {
         .then((response) => {
           this.auditions = response.data;
           console.log(this.auditions);
+          this.isLoading = false;
         })
         .catch((error) => {
           console.log(error);
